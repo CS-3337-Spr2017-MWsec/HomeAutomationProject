7d01301a06acbc4c402d922f5424dc3856063f18
gvgarcia <gvgarcia@hotmail.com>
2017-04-26 18:04:04 -0700

    Lighting
    Fixing size

--- JavaFX_Lighting.java
+++ JavaFX_Lighting.java
@@ -46,7 +46,7 @@ public class JavaFX_Lighting extends Application {
         Lighting lighting = new Lighting();
         lighting.setLight(light);
 
-        
+
         ColorPicker colorPicker = new ColorPicker(Color.YELLOW);
         colorPicker.setOnAction(new EventHandler() {
             @Override
@@ -54,21 +54,21 @@ public class JavaFX_Lighting extends Application {
                 light.setColor(colorPicker.getValue());
             }
         });
- 
-        Button btn = new Button("Button with Lighting");
+
+        Button btn = new Button("Living Room");
         btn.setEffect(lighting);
 
-        
 
-        Circle circle = new Circle(100,  Color.rgb(255,255,255));
+
+        Circle circle = new Circle(30,  Color.rgb(255,255,255));
         circle.setEffect(lighting);
 
 
         HBox hBoxShape = new HBox();
         hBoxShape.getChildren().addAll(circle);
 
-       
-        
+
+
         Label labelElevation = new Label("Light: Darker to Brighter");
         Slider sliderElevation = new Slider();
         sliderElevation.setMin(0);
@@ -79,16 +79,16 @@ public class JavaFX_Lighting extends Application {
         sliderElevation.setShowTickLabels(true);
         sliderElevation.setShowTickMarks(true);
         sliderElevation.valueProperty().addListener(
-                (ObservableValue<? extends Number> ov, 
+                (ObservableValue<? extends Number> ov,
                         Number old_val, Number new_val) -> {
             light.setElevation((Double)new_val);
         });
-              
+
         VBox vBox = new VBox();
         vBox.setPadding(new Insets(10, 10, 10, 10));
-        vBox.getChildren().addAll(colorPicker, btn, hBoxShape, 
+        vBox.getChildren().addAll(colorPicker, btn, hBoxShape,
                 labelElevation, sliderElevation);
-   //2     
+   //2
     	GridPane root2 = new GridPane();
 
       //Light.Distant: Represents a distant light source.
@@ -98,8 +98,8 @@ public class JavaFX_Lighting extends Application {
         light2.setColor(Color.RED);
         Lighting lighting2 = new Lighting();
         lighting2.setLight(light2);
-        
-        
+
+
         ColorPicker colorPicker2 = new ColorPicker(Color.RED);
         colorPicker2.setOnAction(new EventHandler() {
             @Override
@@ -107,13 +107,13 @@ public class JavaFX_Lighting extends Application {
                 light2.setColor(colorPicker2.getValue());
             }
         });
- 
-        Button btn2 = new Button("Button with Lighting");
+
+        Button btn2 = new Button("Bedroom");
         btn2.setEffect(lighting2);
 
-        
 
-        Circle circle2 = new Circle(100,  Color.rgb(255,255,255));
+
+        Circle circle2 = new Circle(30,  Color.rgb(255,255,255));
         circle2.setEffect(lighting2);
 
 
@@ -121,8 +121,8 @@ public class JavaFX_Lighting extends Application {
         HBox hBoxShape2 = new HBox();
         hBoxShape2.getChildren().addAll(circle2);
 
-       
-        
+
+
         Label labelElevation2 = new Label("Light: Darker to Brighter");
         Slider sliderElevation2 = new Slider();
         sliderElevation2.setMin(0);
@@ -133,20 +133,20 @@ public class JavaFX_Lighting extends Application {
         sliderElevation2.setShowTickLabels(true);
         sliderElevation2.setShowTickMarks(true);
         sliderElevation2.valueProperty().addListener(
-                (ObservableValue<? extends Number> ov, 
+                (ObservableValue<? extends Number> ov,
                         Number old_val, Number new_val) -> {
             light2.setElevation((Double)new_val);
         });
-        
-              
+
+
         VBox vBox2 = new VBox();
         vBox2.setPadding(new Insets(10, 10, 10, 10));
-        vBox2.getChildren().addAll(colorPicker2, btn2, hBoxShape2, 
+        vBox2.getChildren().addAll(colorPicker2, btn2, hBoxShape2,
                 labelElevation2, sliderElevation2);
 //2 ends
     	GridPane root3 = new GridPane();
 
-//3  
+//3
         //Light.Distant: Represents a distant light source.
         Light.Distant light3 = new Light.Distant();
         light3.setAzimuth(45);
@@ -155,7 +155,7 @@ public class JavaFX_Lighting extends Application {
         Lighting lighting3 = new Lighting();
         lighting3.setLight(light3);
 
-        
+
         ColorPicker colorPicker3 = new ColorPicker(Color.GREEN);
         colorPicker3.setOnAction(new EventHandler() {
             @Override
@@ -163,14 +163,14 @@ public class JavaFX_Lighting extends Application {
                 light3.setColor(colorPicker3.getValue());
             }
         });
- 
-        Button btn3 = new Button("Button with Lighting");
+
+        Button btn3 = new Button("Hallways");
 
         btn3.setEffect(lighting3);
 
-        
 
-        Circle circle3 = new Circle(100,  Color.rgb(255,255,255));
+
+        Circle circle3 = new Circle(30,  Color.rgb(255,255,255));
         circle3.setEffect(lighting3);
 
 
@@ -178,8 +178,8 @@ public class JavaFX_Lighting extends Application {
         HBox hBoxShape3 = new HBox();
         hBoxShape3.getChildren().addAll(circle3);
 
-       
-        
+
+
         Label labelElevation3 = new Label("Light: Darker to Brighter");
         Slider sliderElevation3 = new Slider();
         sliderElevation3.setMin(0);
@@ -190,22 +190,23 @@ public class JavaFX_Lighting extends Application {
         sliderElevation3.setShowTickLabels(true);
         sliderElevation3.setShowTickMarks(true);
         sliderElevation3.valueProperty().addListener(
-                (ObservableValue<? extends Number> ov, 
+                (ObservableValue<? extends Number> ov,
                         Number old_val, Number new_val) -> {
             light3.setElevation((Double)new_val);
         });
-        
-              
+
+
         VBox vBox3 = new VBox();
         vBox3.setPadding(new Insets(10, 10, 10, 10));
-        vBox3.getChildren().addAll(colorPicker3, btn3, hBoxShape3, 
+        vBox3.getChildren().addAll(colorPicker3, btn3, hBoxShape3,
                 labelElevation3, sliderElevation3);
 //3 ends
-        root.getChildren().add(vBox); //yellow
-        root.setAlignment(Pos.TOP_CENTER);
-        root2.getChildren().add(vBox2);//red
-        root2.setAlignment(Pos.TOP_LEFT);
-        root3.getChildren().add(vBox3); //green
+        root.getChildren().add(vBox); //yellow bottom center
+        root.setAlignment(Pos.BOTTOM_CENTER);
+        root2.getChildren().add(vBox2);//red bottom right
+        root2.setAlignment(Pos.BOTTOM_LEFT);
+        root3.getChildren().add(vBox3); //green bottom left
+        root3.setAlignment(Pos.BOTTOM_LEFT);
 
         primaryStage.show();
 
@@ -213,17 +214,17 @@ public class JavaFX_Lighting extends Application {
 		BorderPane border = new BorderPane(root, null, root2, null, root3);
         primaryStage.setTitle("Light");
 
-		primaryStage.setScene(new Scene(border, 750, 500));
+		primaryStage.setScene(new Scene(border, 500, 500));
 		primaryStage.show();
-        
+
 
     }
-    
+
 
 
 
 	public static void main(String[] args) {
         launch(args);
     }
-    
+
 }
